bffb060dd3d06abe5ddfa4d71474e6f43656bd99
Sahil Ismail <sahil@robotech.co.tz>
2025-12-01 18:30:29 +0300

    updated Physics.py

--- Projects/Physics/Physics.py
+++ Projects/Physics/Physics.py
@@ -20,13 +20,18 @@ space.gravity = 0.0, -100.0
 # create body
 body = pymunk.Body()
 body.position = 500, 800
+body_segment = pymunk.Body(body_type=pymunk.Body.STATIC)
 
 # create shape
 shape = pymunk.Circle(body, 15)
 shape.density = 1
+shape.elasticity = 0.5
+shape_segment = pymunk.Segment(body_segment, (0, 150), (800, 50), 2)
+shape_segment.elasticity = 1
 
 # add to space
 space.add(body, shape)
+space.add(body_segment, shape_segment)
 
 # main loop
 start = True
@@ -42,8 +47,9 @@ while start:
     # display
     x, y = body.position[0], height - body.position[1]
     pygame.draw.circle(window, (0, 0, 255), (int(x), int(y)), 15)
+    pygame.draw.line(window, (0, 0, 0), (8, 650), (800, 750), 5)
 
     # update display
     pygame.display.update()
     clock.tick(fps)
-    pymunk.step(1 / fps)
+    space.step(1 / fps)
